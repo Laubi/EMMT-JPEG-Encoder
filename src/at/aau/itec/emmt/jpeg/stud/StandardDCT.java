6611beff4b89b1daca69412c2b864a96fc334652
David <dlaubrei@gmail.com>
2017-11-20 13:01:53 +0100

    Refactor StandardDCT component

--- src/at/aau/itec/emmt/jpeg/stud/StandardDCT.java
+++ src/at/aau/itec/emmt/jpeg/stud/StandardDCT.java
@@ -1,36 +1,43 @@
 package at.aau.itec.emmt.jpeg.stud;
 
-import at.aau.itec.emmt.jpeg.impl.Block;
 import at.aau.itec.emmt.jpeg.impl.DCTBlock;
 import at.aau.itec.emmt.jpeg.spec.BlockI;
 import at.aau.itec.emmt.jpeg.spec.DCTBlockI;
 import at.aau.itec.emmt.jpeg.spec.DCTI;
 
+import static java.lang.Math.PI;
+import static at.aau.itec.emmt.jpeg.spec.BlockI.N;
+
 public class StandardDCT implements DCTI {
+
+    private static final double oneRootTwo = 1f / Math.sqrt(2);
+
+    private static double C(int pos){
+        return pos == 0 ? oneRootTwo : 1;
+    }
 
     @Override
     public DCTBlockI forward(BlockI b) {
-        int N = BlockI.N;
-        double[] c = new double[BlockI.N];
-        int[][] f;
-        b = levelShift(b);
-        f = b.getData();
-        //initialize Coefficients
-        for (int i = 1; i < c.length; i++) {
-            c[i] = 1;
-        }
-        c[0] = (1 / Math.sqrt(2.0));
+        final int[][] f = levelShift(b);
 
-        double[][] F = new double[N][N];
+        final double[][] F = new double[N][N];
+
         for (int u = 0; u < N; u++) {
             for (int v = 0; v < N; v++) {
-                double sum = 0.0;
+                double c4 = ( C(u) * C(v) ) / 4f;
+
+                double sum = 0;
                 for (int i = 0; i < N; i++) {
                     for (int j = 0; j < N; j++) {
-                        sum += f[i][j] * Math.cos(((2 * i + 1) * u * Math.PI) / 16.0) * Math.cos(((2 * j + 1) * v * Math.PI) / 16.0);
+                        double cos_1 = Math.cos(((2f * i + 1f) * u * PI) / 16f);
+                        double cos_2 = Math.cos(((2f * j + 1f) * v * PI) / 16f);
+
+                        sum += f[i][j] * cos_1 * cos_2;
                     }
                 }
-                sum *= ((c[u] * c[v]) / 4.0);
+
+                sum *= c4;
+
                 F[u][v] = sum;
             }
         }
@@ -38,14 +45,16 @@ public class StandardDCT implements DCTI {
     }
 
 
-    private BlockI levelShift(BlockI b) {
+    private int[][] levelShift(BlockI b) {
         int[][] data = b.getData();
+
         for (int i = 0; i < BlockI.N; i++) {
             for (int j = 0; j < BlockI.N; j++) {
-                data[i][j] = data[i][j] - 128;
+                data[i][j] -= 128;
             }
         }
-        return new Block(data);
+
+        return data;
     }
 
 
