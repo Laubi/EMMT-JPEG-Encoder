c55130211078678321193ad90a72414997d2c73c
David <dlaubrei@gmail.com>
2017-11-20 12:43:43 +0100

    Reformat code

--- src/at/aau/itec/emmt/jpeg/stud/StandardDCT.java
+++ src/at/aau/itec/emmt/jpeg/stud/StandardDCT.java
@@ -10,46 +10,43 @@ public class StandardDCT implements DCTI {
 
     @Override
     public DCTBlockI forward(BlockI b) {
-        int N=BlockI.N; 
-        double [] c= new double[BlockI.N];
-    	int [][] f; 
-        b=levelShift(b); 
-        f=b.getData(); 
-    	//initialize Coefficients
-    	for (int i=1;i<c.length;i++) {
-                c[i]=1;
-            }
-        c[0]=(1/Math.sqrt(2.0));
-    	
+        int N = BlockI.N;
+        double[] c = new double[BlockI.N];
+        int[][] f;
+        b = levelShift(b);
+        f = b.getData();
+        //initialize Coefficients
+        for (int i = 1; i < c.length; i++) {
+            c[i] = 1;
+        }
+        c[0] = (1 / Math.sqrt(2.0));
+
         double[][] F = new double[N][N];
-        for (int u=0;u<N;u++) {
-          for (int v=0;v<N;v++) {
-            double sum = 0.0;
-            for (int i=0;i<N;i++) {
-              for (int j=0;j<N;j++) {
-            	  sum+=f[i][j]*Math.cos(((2*i+1)*u*Math.PI)/16.0)*Math.cos(((2*j+1)*v*Math.PI)/16.0);
-              }
+        for (int u = 0; u < N; u++) {
+            for (int v = 0; v < N; v++) {
+                double sum = 0.0;
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
+                        sum += f[i][j] * Math.cos(((2 * i + 1) * u * Math.PI) / 16.0) * Math.cos(((2 * j + 1) * v * Math.PI) / 16.0);
+                    }
+                }
+                sum *= ((c[u] * c[v]) / 4.0);
+                F[u][v] = sum;
             }
-            sum*=((c[u]*c[v])/4.0);
-            F[u][v]=sum;
-          }
         }
         return new DCTBlock(F);
     }
-    
-    
-    private BlockI levelShift(BlockI b)
-    {
-    	int [][] data=b.getData(); 
-    	for(int i=0; i<BlockI.N; i++)
-    	{
-    		for(int j=0; j<BlockI.N; j++)
-    		{
-    			data[i][j]=data[i][j]-128; 
-    		}
-    	}
-    	return new Block(data); 
+
+
+    private BlockI levelShift(BlockI b) {
+        int[][] data = b.getData();
+        for (int i = 0; i < BlockI.N; i++) {
+            for (int j = 0; j < BlockI.N; j++) {
+                data[i][j] = data[i][j] - 128;
+            }
+        }
+        return new Block(data);
     }
-    
-    
+
+
 }
